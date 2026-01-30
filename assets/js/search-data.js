178b5e1691701578a107075752f97413b2e18c4a
kamrulkonok <129620441+kamrulkonok@users.noreply.github.com>
2026-01-30 06:55:20 +0000

    Deploying to gh-pages from @ kamrulkonok/kamrulkonok.github.io@8e907b06849315810b1162f545dd92d00f08b1d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "research",
-          description: "This page showcases my research contributions (pre-print version) and ongoing work in the areas of Foundation Models, Retrieval-Augmented Generation (RAG), Geometric Deep Learning, Medical Imaging, and Knowledge Graphs. While I continue to develop and refine my research, I am actively working on several projects that will be published in the near future.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "A collection of my projects.",
           section: "Navigation",
